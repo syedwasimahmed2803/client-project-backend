96d04cf41d41121b7618259e72f32187e495394f
wasimionage <wasim.a@ionage.in>
2025-05-25 17:48:51 +0530

    Initial commit - backend setup

---
+++ src/config/db.js
@@ -0,0 +1,5 @@
+// src/config/db.js
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
